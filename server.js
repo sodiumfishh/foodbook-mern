f20d7b705e827437aad932635fa539913299de7d
Sami Nafis <sami.nafis2428@gmail.com>
2021-09-28 16:54:04 +0600

    VERSION 1.0

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/recipes", recipeRouter)
 // Serve static assets if in production
 if(process.env.NODE_ENV === "production") {
 	//Set static folder
-	app.use(express.static('frontend/build'))
+  	app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
